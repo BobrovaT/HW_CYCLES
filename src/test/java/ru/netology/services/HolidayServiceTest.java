ed88a0db3c0f40bc9fc4f80ccd9e475eac7675ba
BobrovaT <bobrova.t.1987@gmail.com>
2025-08-30 22:52:50 +0300

    CsvFile for CounterHolidays

--- src/test/java/ru/netology/services/HolidayServiceTest.java
+++ src/test/java/ru/netology/services/HolidayServiceTest.java
@@ -1,14 +1,19 @@
 package ru.netology.services;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class HolidayServiceTest {
 
-    @Test
-    public void shouldCalcMonth() {
+    @ParameterizedTest
+    @CsvFileSource (files = "src/test/resources/conditions.csv")
+
+    public void shouldCalcMonth(int income, int expenses, int threshold) {
         HolidayService service = new HolidayService();
 
-        int calc = service.calculate(10_000, 3_000, 20_000);
+        int calc = service.calculate(income, expenses, threshold);
         System.out.println(calc);
     }
 }
